bf6a24bfe49670df93c64840091fbcd70951e0a5
Darkhan Almakhan <almakhan@group-ib.com>
2020-05-07 17:50:06 +0300

    teldrassil refactoring

--- main.py
+++ main.py
@@ -1,41 +1,15 @@
-import logging
-
+from os import path
 from flask import Flask
 from flask import jsonify
-from flask import send_file
+from mirror_app.view import mirror_blueprint
+from logging.config import fileConfig
 
-from libs.common import check_dir_existence, check_file_existence
-
-errors = [404, 406, 403]
+log_file_path = path.join(path.dirname(path.abspath(__file__)), 'logging.cfg')
 
 app = Flask(__name__)
+app.register_blueprint(mirror_blueprint)
 
-logger = logging.getLogger('gunicorn.error')
-logfile = "/var/log/teldrassil/teldrassil-api.log"
-handler = logging.FileHandler(logfile)
-fmtr = logging.Formatter('%(asctime)s - %(message)s', datefmt='[%Y-%b-%d %H:%M]')
-handler.setFormatter(fmtr)
-logger.addHandler(handler)
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def proxy(path):
-    base_dir_path = list(path.rsplit('/', 1))
-    base_dir_path.remove('download')
-    base_dir_path = ' '.join(map(str, base_dir_path))
-    base_dir_path = 'packages/{}'.format(base_dir_path)
-    base_package_path = 'packages/{}'.format(path)
-
-    message, status_code = check_dir_existence(base_dir_path)
-    if status_code in errors:
-        return jsonify(error_message=message), status_code
-
-    message, status_code = check_file_existence(path)
-    if status_code in errors:
-        return jsonify(error_message=message), status_code
-
-    return send_file(base_package_path)
+fileConfig(log_file_path)
 
 
 @app.route('/check')

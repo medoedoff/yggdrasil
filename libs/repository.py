30dd52a550e57bb898f17dfaf04c7124f23f4fb2
Darkhan Almakhan <almakhan@group-ib.com>
2020-05-18 12:28:19 +0300

    first part of lib

--- libs/repository.py
+++ libs/repository.py
@@ -1,15 +1,28 @@
+import os
+import json
+
+
 class Index:
-    def __init__(self, package_name):
+    def __init__(self, package_name, package_info, index_path):
         """
         :param package_name: str package name
+        :param index_path: str path to index directory
         """
         self.package_name = package_name.lower()
+        self.index_path = index_path
+        self.package_info = package_info
 
-    def _update(self):
-        pass
+    def _update(self, package_index_path):
+        path_to_save = f'{package_index_path}'
+        package_info = self.package_info
+        with open(path_to_save, 'a') as json_file:
+            json_file.write(json.dumps(package_info))
 
-    def _create(self):
-        pass
+    def _create(self, package_index_path):
+        path_to_save = f'{package_index_path}'
+        package_info = self.package_info
+        with open(path_to_save, 'w') as json_file:
+            json_file.write(json.dumps(package_info))
 
     def _divide_string(self):
         """
@@ -28,5 +41,13 @@ class Index:
             divided_string = f'{package_name[0:2]}/{package_name[2:4]}/{package_name}'
             return divided_string
 
-    def is_exist(self):
-        pass
+    def synchronise(self):
+        package_index = self._divide_string()
+        index_path = self.index_path
+        package_name = self.package_name
+        package_index_path = f'{index_path}/{package_index}/{package_name}'
+        if os.path.isfile(package_index_path) is False:
+            self._create()
+        else:
+            self._update()
+        return

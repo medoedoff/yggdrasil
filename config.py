0e4a37a55669c524edf4a1b9eb94116fae8393a9
Darkhan Almakhan <almakhan@group-ib.com>
2020-05-25 17:53:48 +0300

    config doesnt work

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True

595d500bc7aafef5c925d3218a59a3c99e1e684e
Darkhan Almakhan <almakhan@group-ib.com>
2020-06-03 17:41:49 +0300

    token required decorator

--- application/auth.py
+++ application/auth.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, request, render_template, jsonify, redirect, url_for
+from flask import Blueprint, request, render_template, jsonify, redirect, url_for, make_response
 from flask_security import Security
 from flask_security.utils import get_hmac, _pwd_context
 
-from .models import Users
+from functools import wraps
+
+from .models import Users, jwt
 
 from http import HTTPStatus
 
@@ -13,8 +15,29 @@ security = Security()
 unauthorized_message = 'Could not verify'
 
 
-@login_blueprint.route('/', methods=['GET', 'POST'])
-def login():
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth_token = request.cookies.get('auth_token', None)
+        if auth_token is not None:
+            try:
+                public_id = Users.decode_auth_token(auth_token)
+            except (jwt.ExpiredSignatureError, jwt.InvalidTokenError):
+                return False
+            current_user = Users.query.filter_by(public_id=public_id).first()
+            return f(current_user, *args, **kwargs)
+        else:
+            return False
+    return decorated
+
+
+@login_blueprint.route('/', methods=['GET'])
+def login_get():
+    return render_template('login/index.html')
+
+
+@login_blueprint.route('/', methods=['POST'])
+def login_post():
     if request.method == 'POST':
         email_form = request.form['email']
         password_form = request.form['password']
@@ -27,8 +50,10 @@ def login():
         verify = _pwd_context.verify(password_form, user.password)
 
         if verify:
-            return redirect(url_for('admin'))
+            auth_token = user.encode_auth_token(user.public_id)
+            if auth_token:
+                response = make_response(redirect(url_for('admin.index')))
+                response.set_cookie('auth_token', auth_token)
+                return response
         else:
             return jsonify(message=unauthorized_message), HTTPStatus.UNAUTHORIZED.value
-
-    return render_template('login.html')

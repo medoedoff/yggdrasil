ff4f57a3de63ff4516325c3f356414887fff7f64
Darkhan Almakhan <almakhan@group-ib.com>
2020-06-03 17:41:12 +0300

    added authentication

--- application/admin.py
+++ application/admin.py
@@ -1,11 +1,19 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, AdminIndexView
+from flask import redirect, url_for
+
+from .auth import token_required
 
 admin = Admin(name='GIB-Teldrassil')
 
 
 class MyAdminIndexView(AdminIndexView):
-    pass
+    @token_required
+    def is_accessible(self, current_user):
+        return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login_auth.login_get'))
 
 
 class MyModelView(ModelView):
